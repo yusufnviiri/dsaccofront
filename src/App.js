1201e9ae239f10ed3750776dbccd20c9206934e6
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-06-06 04:52:56 +0300

    set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashbord from './components/Dashbord';
+
+function App() {
+  return (
+    <div className="App font-mul">
+      <Dashbord/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dashbord.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Dashbord() {
+  return (
+    <>
+    <div><h1 className='text-green-900 '> The best app ever
+        </h1></div>
+    
+    
+    </>
+  )
+}
+
+export default Dashbord
