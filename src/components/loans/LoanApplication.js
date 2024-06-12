361227b413a7616668e2d7d6d5748158abedddef
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-06-12 16:42:54 +0300

    loan application

--- src/components/loans/LoanApplication.js
+++ src/components/loans/LoanApplication.js
@@ -14,17 +14,24 @@ function LoanApplication() {
   useEffect(() => {
     dispatch(loanTypes());
   }, []);
-
   const [principleAmount, setprincipleAmount] = useState(0);
   const [loanType, setloanType] = useState(0);
-  
-  const depositDetails = {
-    principleAmount,
+  const [security,setsecurity]=useState("N/A");
+  const [loanPeriod,setloanPeriod]=useState("");
+  const [firstWitnessName,setfirstWitnessName]=useState("");
+  const [firstWitnessAddress,setfirstWitnessAddress]=useState("");
+  const [firstWitnessContact,setfirstWitnessContact]=useState("");
+  const [secondWitnessName,setsecondWitnessName]=useState("");
+  const [secondWitnessAddress,setsecondWitnessAddress]=useState("");
+  const [secondWitnessContact,setsecondWitnessContact]=useState("");
+
+  const loanDetails = {
+    principleAmount,security,loanPeriod,firstWitnessAddress,firstWitnessContact,firstWitnessName,secondWitnessAddress,secondWitnessContact,secondWitnessName,
     loanType,
   };
-  const saveDeposit = (e) => {
+  const saveLoan = (e) => {
     e.preventDefault();
-    dispatch(memberDeposit(depositDetails));
+    dispatch(memberDeposit(loanDetails));
     if (notification !== "") {
       navigate("/accounts", { replace: true });
     }
@@ -33,10 +40,10 @@ function LoanApplication() {
     <>
       <div className="w-[30%] m-auto  login_form">
         <h4 className=" my-6 font-bold   underline-offset-2  
-     text-center font-lobs "  >  Deposit </h4>   
+     text-center font-lobs "  > Loan Application </h4>   
           <form
           onSubmit={(e) => {
-            saveDeposit(e);
+            saveLoan(e);
           }}
         >
           <div className="mb-1  ">
@@ -66,6 +73,7 @@ function LoanApplication() {
               )}
             </select>
           </div>
+
           <div className="mb-1  ">
             <label className="font-bold   ml-2 block">Amount</label>
             <input
@@ -78,6 +86,109 @@ function LoanApplication() {
               type="number"
             />
           </div>
+          <div className="mb-1  ">
+            <label className="font-bold   ml-2 block">Security</label>
+            <input
+              required
+              value={security}
+              onChange={(e) => {
+                setsecurity(e.target.value);
+              }}
+              className=" w-full   py-3 text-center"
+              type="text"
+            />
+          </div>
+          <div className="mb-1  ">
+            <label className="font-bold   ml-2 block">Loan Period</label>
+            <input
+              required
+              value={loanPeriod}
+              onChange={(e) => {
+                setloanPeriod(e.target.value);
+              }}
+              className=" w-full   py-3 text-center"
+              type="number"
+            />
+          </div>
+
+          <h4 className=" my-2 font-bold   text-[0.77em] 
+     text-center font-lobs "  > First Witness  </h4>   
+          <div className="mb-1  ">
+            <label className="font-bold   ml-2 block">Name</label>
+            <input
+              required
+              value={firstWitnessName}
+              onChange={(e) => {
+                setfirstWitnessName(e.target.value);
+              }}
+              className=" w-full   py-3 text-center"
+              type="text"
+            />
+          </div>
+          <div className="mb-1  ">
+            <label className="font-bold   ml-2 block">Contact</label>
+            <input
+              required
+              value={firstWitnessContact}
+              onChange={(e) => {
+                setfirstWitnessContact(e.target.value);
+              }}
+              className=" w-full   py-3 text-center"
+              type="text"
+            />
+          </div> <div className="mb-1  ">
+            <label className="font-bold   ml-2 block">Address</label>
+            <input
+              required
+              value={firstWitnessAddress}
+              onChange={(e) => {
+                setfirstWitnessAddress(e.target.value);
+              }}
+              className=" w-full   py-3 text-center"
+              type="text"
+            />
+          </div>
+          <h4 className=" my-2 font-bold   text-[0.77em] 
+     text-center font-lobs "  > Second Witness  </h4>   
+          <div className="mb-1  ">
+            <label className="font-bold   ml-2 block">Name</label>
+            <input
+              required
+              value={secondWitnessName}
+              onChange={(e) => {
+                setsecondWitnessName(e.target.value);
+              }}
+              className=" w-full   py-3 text-center"
+              type="text"
+            />
+          </div>
+          <div className="mb-1  ">
+            <label className="font-bold   ml-2 block">Contact</label>
+            <input
+              required
+              value={secondWitnessContact}
+              onChange={(e) => {
+                setsecondWitnessContact(e.target.value);
+              }}
+              className=" w-full   py-3 text-center"
+              type="text"
+            />
+          </div> <div className="mb-1  ">
+            <label className="font-bold   ml-2 block">Address</label>
+            <input
+              required
+              value={secondWitnessAddress}
+              onChange={(e) => {
+                setsecondWitnessAddress(e.target.value);
+              }}
+              className=" w-full   py-3 text-center"
+              type="text"
+            />
+          </div>
+          
+          
+          
+      
 
           <div
             className="mt-6 text-white submit   font-bold w-full m-auto
