3ff07bec2e805fc659c95bec848250283016a8b5
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-06-13 22:16:36 +0300

    styling

--- src/components/Dashbord.js
+++ src/components/Dashbord.js
@@ -2,6 +2,8 @@ import React,{useEffect} from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
 import {useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
 
 
 function Dashbord() {
@@ -23,15 +25,28 @@ const logOut=()=>{
   localStorage.clear();
   navigate("/", { replace: true });
 }
+let nullValue= JSON.stringify(localStorage.getItem("token"))
+var userToken = JSON.stringify(localStorage.getItem("bearer"));
+var obj=[]
+if(userToken !==nullValue){
+console.log(userToken)
+const decoded = jwtDecode(userToken );
 
+      obj =Object.values(decoded)}
+  
   return (
     <>
     <menu className='bg-emerald-950 w-full py-2 text-[0.9em]  text-stone-300 flex  justify-between font-robotoCo'>
     
       <Link className=' mx-2 text-stone-100 font-mul tracking-wider' to='/'>Dixieland Traders Savings Group</Link>
-
+      {obj.length>0?( <div>   <h2>Name: {obj[1]}</h2></div>
+):(  <h2>User: none</h2>)}
       <nav>
+       
 
+
+        
+       
  
   <Link className=' mr-2' to='/register'>Sign Up</Link>
   <Link className=' mr-2' to='/logins'>Sign In</Link>
