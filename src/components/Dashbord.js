901ae37c97f3df8aea6d2e970fc8841ccb787f57
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-06-13 15:29:56 +0300

    clean up

--- src/components/Dashbord.js
+++ src/components/Dashbord.js
@@ -37,18 +37,13 @@ const logOut=()=>{
 <button  onClick={()=>logOut()} className='text-red-500 uppercase hover:text-yellow-500'>Sign Out</button>
     </menu>
     <div  className=''>      
-      <h1 className='text-green-900  '> The best app ever
-        </h1>
+     
 
         <h4 className='text-red-900  '>Login status     {logginError}
         </h4>
-        <div className='w-1/3 flex justify-between  '>
-          <button className='bg-green-700 px-3 py-1 my-1 rounded text-stone-200' onClick={()=>dispatch(add())}>ADD</button>
-   
-        </div>
+
         
-        <h1 className='text-slate-800  '>This is dashboard
-        </h1></div>
+     </div>
     
     
     </>
